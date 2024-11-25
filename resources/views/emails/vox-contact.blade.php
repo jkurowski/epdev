4b175383fdb8aab6db777f4f673f6ebff2cf4e1a
Miloszel <m.branewicz@wp.pl>
2024-11-25 13:08:21 +0100

    Usunięcie placeholdera z voxmaila

--- resources/views/emails/vox-contact.blade.php
+++ resources/views/emails/vox-contact.blade.php
@@ -40,18 +40,4 @@
     @foreach ($data['agreements'] as $item)
         {{ strtoupper(isset($item['title']) ? $item['title'] : 'Brak tytułu') }} &nbsp; ({{ isset($item['description']) ? $item['description'] : 'Brak opisu' }}) <br>
     @endforeach
-
-    ZGODA_MARKETING (Wyrażam zgodę na przetwarzanie przez DANE_SPÓŁKI_I_ADRES_SIEDZIBY, moich danych osobowych zawartych
-    w niniejszym formularzu kontaktowym w celu i zakresie koniecznym do realizacji zgłoszenia.)
-    ZGODA_MAIL (Wyrażam zgodę na przetwarzanie przez DANE_SPÓŁKI_I_ADRES_SIEDZIBY, moich danych osobowych zawartych w
-    niniejszym formularzu kontaktowym w celu przesyłania mi ofert handlowych na produkty własne spółki drogą
-    elektroniczną.)
-    ZGODA_TEL (Wyrażam zgodę na przetwarzanie przez DANE_SPÓŁKI_I_ADRES_SIEDZIBY, moich danych osobowych zawartych w
-    niniejszym formularzu kontaktowym w celu kontaktu telefonicznego ze strony przedstawicieli spółki w sprawach
-    związanych z ofertą handlową na produkty własne.)
-    ZGODA_OSWIADCZENIE (Administratorem Państwa danych osobowych jest firma DANE_SPÓŁKI_I_ADRES_SIEDZIBY. Dane wpisane w
-    formularzu kontaktowym będą przetwarzane w celu udzielenia odpowiedzi na przesłane zapytanie zgodnie z Polityką
-    Prywatności.)
-    ZGODA_SMS (Wyrażam zgodę na przetwarzanie przez DANE_SPÓŁKI_I_ADRES_SIEDZIBY, moich danych osobowych zawartych w
-    niniejszym formularzu kontaktowym w celu przesyłania mi ofert handlowych na produkty własne spółki drogą SMS.)
 @endif
