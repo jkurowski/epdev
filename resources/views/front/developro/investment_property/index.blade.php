fb8e052d04ecc2efcd7304e4d7c2710538d5c0b3
Jacek <kontakt@jk-design.pl>
2025-01-21 13:01:44 +0100

    [Jace][Fix] Favicon

--- resources/views/front/developro/investment_property/index.blade.php
+++ resources/views/front/developro/investment_property/index.blade.php
@@ -176,18 +176,22 @@
                     <div class="vr"></div>
                 @endif
                 {{-- Price --}}
-                <div class="box price d-none">
+                <div class="box price">
                     @if ($property->ask_for_price)
                         <a href="#contact-form">Zapytaj o cenę</a>
                     @else
-                    <div class="title">Cena:</div>
-                    <div class="data">
-                        @if ($property->promotion_price)
-                            @money($property->promotion_price)
-                        @else
-                            @money($property->price)
+                        @if ($property->price > 0)
+                            <div class="title">Cena:</div>
+                            <div class="data">
+                                @if ($property->promotion_price)
+                                    @money($property->promotion_price)
+                                @else
+                                    @money($property->price)
+                                @endif
+                            </div>
+                            <div class="vr"></div>
                         @endif
-                    </div>
+                        <div class="vr"></div>
                     @endif
                 </div>
 
@@ -198,7 +202,6 @@
                             $pdf_url = $property->file_pdf;
                         }
                     @endphp
-                    <div class="vr d-none"></div>
                     {{-- PDF --}}
                     <div class="box pdf">
                         <a class="btn btn-underline " href="{{ $pdf_url }}" target="_blank">
