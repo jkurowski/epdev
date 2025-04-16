ac5bfc93931d3d14874df95fb4757151d678dec1
Jacek Kurowski <jacek@4dl.pl>
2025-04-16 16:34:21 +0200

    [Jace][Fix] Możliwość zmiany meta tagów

--- resources/views/components/form-cities.blade.php
+++ resources/views/components/form-cities.blade.php
@@ -5,8 +5,8 @@
         <div class="form-subtitle mt-3">{{ $label }}</div>
         @foreach ($cities as $value => $city)
             <div class="form-check text-start pt-3 ps-0">
-                <input class="form-check-input" type="radio" id="city-{{ $value }}" name="city" value="{{ $value }}">
-                <label class="form-check-label small fw-medium ms-2" for="city-{{ $value }}">{{ $city }}</label>
+                <input class="form-check-input" type="radio" id="city-{{ \Illuminate\Support\Str::slug($value) }}" name="city" value="{{ $value }}">
+                <label class="form-check-label small fw-medium ms-2" for="city-{{ \Illuminate\Support\Str::slug($value) }}">{{ $city }}</label>
             </div>
         @endforeach
     </div>
