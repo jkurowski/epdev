e46ea7dd5b4167e3f7f3bc7e8e61c160ff77539e
Jacek Kurowski <jacek@4dl.pl>
2025-04-17 00:19:48 +0200

    [Jace][Fix] Możliwość zmiany meta tagów

--- resources/views/front/developro/investment_shared/list.blade.php
+++ resources/views/front/developro/investment_shared/list.blade.php
@@ -8,7 +8,7 @@
                     <li class="grid-btn"
                         id="row-tab"
                         role="tab"
-                        aria-controls="pills-row"
+                        aria-controls="row-tab"
                         aria-selected="true"
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" width="53" height="28" viewBox="0 0 53 28"><g transform="translate(-1521 -3777)"><g transform="translate(-73 -32)"><g transform="translate(1594 3831)" fill="none" stroke="#000" stroke-width="1"><rect width="53" height="6" rx="2" stroke="none"/><rect x="0.5" y="0.5" width="52" height="5" rx="1.5" fill="none"/></g><g transform="translate(1594 3820)" fill="none" stroke="#000" stroke-width="1"><rect width="53" height="6" rx="2" stroke="none"/><rect x="0.5" y="0.5" width="52" height="5" rx="1.5" fill="none"/></g><g transform="translate(1594 3809)" fill="none" stroke="#000" stroke-width="1"><rect width="53" height="6" rx="2" stroke="none"/><rect x="0.5" y="0.5" width="52" height="5" rx="1.5" fill="none"/></g></g></g></svg>
@@ -17,7 +17,7 @@
                     <li class="grid-btn active"
                         id="column-tab"
                         role="tab"
-                        aria-controls="pills-column"
+                        aria-controls="column-tab"
                         aria-selected="false"
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" width="39" height="28" viewBox="0 0 39 28"><g transform="translate(-1594 -3777)"><g transform="translate(1594 3793)" fill="none" stroke="#000" stroke-width="1"><rect width="11" height="12" rx="2" stroke="none"/><rect x="0.5" y="0.5" width="10" height="11" rx="1.5" fill="none"/></g><g transform="translate(1594 3777)" fill="none" stroke="#000" stroke-width="1"><rect width="11" height="12" rx="2" stroke="none"/><rect x="0.5" y="0.5" width="10" height="11" rx="1.5" fill="none"/></g><g transform="translate(1608 3793)" fill="none" stroke="#000" stroke-width="1"><rect width="11" height="12" rx="2" stroke="none"/><rect x="0.5" y="0.5" width="10" height="11" rx="1.5" fill="none"/></g><g transform="translate(1608 3777)" fill="none" stroke="#000" stroke-width="1"><rect width="11" height="12" rx="2" stroke="none"/><rect x="0.5" y="0.5" width="10" height="11" rx="1.5" fill="none"/></g><g transform="translate(1622 3793)" fill="none" stroke="#000" stroke-width="1"><rect width="11" height="12" rx="2" stroke="none"/><rect x="0.5" y="0.5" width="10" height="11" rx="1.5" fill="none"/></g><g transform="translate(1622 3777)" fill="none" stroke="#000" stroke-width="1"><rect width="11" height="12" rx="2" stroke="none"/><rect x="0.5" y="0.5" width="10" height="11" rx="1.5" fill="none"/></g></g></svg>
