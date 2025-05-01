fea6cefbff404e56bddfe8ed43f6ff9213948331
Jacek Kurowski <jacek@4dl.pl>
2025-05-01 10:49:58 +0200

    [Jace][Fix] Wyszukiwarka

--- resources/views/layouts/partials/inline.blade.php
+++ resources/views/layouts/partials/inline.blade.php
@@ -150,8 +150,9 @@
                                     content_css: 'default',
                                     branding: false,
                                     height: 400,
+                                    menubar: false,
                                     plugins: "searchreplace autolink directionality visualblocks visualchars fullscreen image link gallery media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern filemanager",
-                                    toolbar1: "formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | removeformat | gallery",
+                                    toolbar1: "formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | removeformat",
                                     relative_urls: false,
                                     image_advtab: true,
                                     auto_focus: 'modaleditortext'
