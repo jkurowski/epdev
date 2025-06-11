44fe3e218b5f3a36b3b194b11d46bcf166390be4
Jacek Kurowski <jacek@4dl.pl>
2025-06-11 14:48:54 +0200

    [Jace][Fix] Usuniecie klasy pretty

--- resources/views/form-elements/tinymce-small.blade.php
+++ resources/views/form-elements/tinymce-small.blade.php
@@ -1,4 +1,3 @@
-
 <script>
     tinymce.init({
         selector: ".tinymce",
@@ -18,6 +17,12 @@
         image_advtab: true,
         external_filemanager_path:"/js/editor/plugins/filemanager/",
         filemanager_title:"kCMS Filemanager" ,
-        external_plugins: { "filemanager" : "{{ asset('/js/editor/plugins/filemanager/plugin.min.js') }}"}
+        external_plugins: { "filemanager" : "{{ asset('/js/editor/plugins/filemanager/plugin.min.js') }}"},
+        valid_elements: '*[*]', // allow all elements/attributes
+        extended_valid_elements: 'svg[*],g[*],path[*],circle[*],rect[*],line[*],polyline[*],polygon[*],ellipse[*],text[*]',
+        valid_children: "+body[svg|g|path|circle|rect|line|polyline|polygon|ellipse|text]",
+        forced_root_block: false, // prevent <p> wrapping SVG
+        verify_html: false,
+        content_style: 'svg { max-width: 100%; height: auto; }'
     });
 </script>
