cfb322ea0be4027a12356626350de5467e3ed824
Jacek Kurowski <jacek@4dl.pl>
2025-06-12 14:47:52 +0200

    [Jace][Fix] Usuniecie klasy pretty

--- resources/views/front/developro/investment/show.blade.php
+++ resources/views/front/developro/investment/show.blade.php
@@ -212,6 +212,7 @@
         </div>
         @endif
 
+        @if($investment->end_content)
         <div class="container">
             <div class="row">
                 <div class="col-12">
@@ -219,6 +220,7 @@
                 </div>
             </div>
         </div>
+        @endif
 
         @if ($investment->gallery && $investment->gallery->photos->count() > 0)
             {{-- Gallery --}}
